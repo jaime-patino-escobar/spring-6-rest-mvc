fd153488e3bb9a71c93e4d0afc5a29a0e02b5756
Jaime Patino <jaime.patino.escobar@gmail.com>
2023-05-09 15:30:14 -0500

    CSV File uploads

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -6,9 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
+import java.sql.SQLType;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -26,7 +32,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
@@ -49,6 +56,10 @@ public class Beer {
 
     @NotNull
     private BigDecimal price;
+    
+    @CreationTimestamp
     private LocalDateTime createdDate;
+    
+    @UpdateTimestamp
     private LocalDateTime updateDate;
 }
